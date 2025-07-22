7ba0f4a7ea84787add054450409bb9fbaad3bbb8
V0ix <142316886+V0ix@users.noreply.github.com>
2025-07-22 13:48:26 +0530

    Create docker_hub.groovy

---
+++ vars/docker_hub.groovy
@@ -0,0 +1,9 @@
+def docker(){
+  echo 'pushing the image to Docker Hub'
+                withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', 
+                                  usernameVariable: 'DOCKER_USER', 
+                                  passwordVariable: 'DOCKER_PASS')]) {
+                sh 'docker tag nodes_app:latest "$DOCKER_USER"/nodes_app:latest'
+                sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
+                sh 'docker logout'
+}

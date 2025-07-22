705a4e94f17533db3f1175ba9154d5ceaf376d8b
V0ix <142316886+V0ix@users.noreply.github.com>
2025-07-22 20:03:30 +0530

    Update docker_hub.groovy

--- vars/docker_hub.groovy
+++ vars/docker_hub.groovy
@@ -1,9 +1,14 @@
-def call(){
-  echo 'pushing the image to Docker Hub'
-                withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', 
-                                  usernameVariable: 'DOCKER_USER', 
-                                  passwordVariable: 'DOCKER_PASS')]) {
-                sh 'docker tag nodes_app:latest "$DOCKER_USER"/nodes_app:latest'
-                sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
-                sh 'docker logout'
+def call() {
+    echo 'pushing the image to Docker Hub'
+    
+    withCredentials([usernamePassword(
+        credentialsId: 'docker-hub-creds', 
+        usernameVariable: 'DOCKER_USER', 
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh 'docker tag nodes_app:latest "$DOCKER_USER"/nodes_app:latest'
+        sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
+        sh 'docker push "$DOCKER_USER"/nodes_app:latest'
+        sh 'docker logout'
+    }
 }

9bbd1989c63465ba8dbd24eccfc1abd68be22ccd
V0ix <142316886+V0ix@users.noreply.github.com>
2025-07-22 13:28:14 +0530

    Update cloning.groovy

--- vars/cloning.groovy
+++ vars/cloning.groovy
@@ -1,3 +1,5 @@
+def clone(String url , String branch){
 echo "cloning the code from the github"
-git url: 'https://github.com/LondheShubham153/django-notes-app', branch: 'main'
+git url: "${url}", branch: "${branch}"
 echo "cloning successful"
+}

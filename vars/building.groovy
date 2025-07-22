7d416bb7b146f856af9a2ed0b54223435cd73ddb
V0ix <142316886+V0ix@users.noreply.github.com>
2025-07-22 19:51:14 +0530

    Update building.groovy

--- vars/building.groovy
+++ vars/building.groovy
@@ -1,4 +1,4 @@
-def build(String tag){
+def call(String tag){
     echo "building the docker image from the Dockerfile"
     sh "docker build -t '${tag}' ."
     echo "Docker image building was successful"

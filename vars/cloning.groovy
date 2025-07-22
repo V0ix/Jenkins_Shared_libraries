34504563e0f4687a11435e11a541cf4841740cf9
V0ix <142316886+V0ix@users.noreply.github.com>
2025-07-22 19:51:45 +0530

    Update cloning.groovy

--- vars/cloning.groovy
+++ vars/cloning.groovy
@@ -1,4 +1,4 @@
-def clone(String url , String branch){
+def call(String url , String branch){
 echo "cloning the code from the github"
 git url: "${url}", branch: "${branch}"
 echo "cloning successful"
